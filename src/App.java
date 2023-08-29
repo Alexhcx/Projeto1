49048b53747153af1ea36d4abf19b0de54817611
Alexandre <6085319+Alexhcx@users.noreply.github.com>
2023-08-29 13:24:21 -0300

    projeto1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Projetos e projetos");
+
+        String br = "Brasil";
+        int ano = 1992;
+
+        String resultado =  br + Integer.toString(ano);
+
+        System.out.println(resultado);
+
     }
 }
