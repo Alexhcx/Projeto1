15eb3c7e162f1353addb8b572680e56581ee39b2
Alexandre <6085319+Alexhcx@users.noreply.github.com>
2023-08-29 14:40:48 -0300

    Projeto1

--- src/App.java
+++ src/App.java
@@ -2,12 +2,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Projetos e projetos");
 
-        String br = "Brasil";
+        final String br = "Brasil";
         int ano = 1992;
 
         String resultado =  br + Integer.toString(ano);
 
         System.out.println(resultado);
+        System.out.println(nomeCompleto("Alexandre", "Nazareth"));
+    }
 
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+
+        primeiroNome = primeiroNome.toUpperCase();
+        segundoNome = segundoNome.toUpperCase();
+
+        String nome = primeiroNome + " " + segundoNome; 
+
+        return nome;
     }
 }
